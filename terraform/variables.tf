78cc7c1a918cb1d1ffb781eaf8bcff291e3e7c43
bharathiraja-15 <bharathimtech11@gmail.com>
2026-03-15 13:39:59 +0530

    Change default AWS region to 'eu-east-1'

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy the EKS cluster"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-east-1"
 }
 
 variable "environment" {
